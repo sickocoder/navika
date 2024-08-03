8f7c7afc50557a7ce3b8e38755596fa14cb90827
José Tomé <jose.tome@standardbank.co.ao>
2024-08-03 22:30:55 +0100

    Initial Commit

---
+++ Navika/NavikaApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavikaApp.swift
+//  Navika
+//
+//  Created by A255085 on 03/08/24.
+//
+
+import SwiftUI
+
+@main
+struct NavikaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
